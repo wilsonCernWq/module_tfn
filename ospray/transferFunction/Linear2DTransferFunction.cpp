1fabc689503ced285daebb5c7c74976f7e4ca566
Qi WU <qwu@kepler.sci.utah.edu>
2017-11-08 16:44:52 -0700

    add fields in 2D transfer function

---
+++ ospray/transferFunction/Linear2DTransferFunction.cpp
@@ -0,0 +1,87 @@
+// ======================================================================== //
+// Copyright 2009-2017 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#include "transferFunction/Linear2DTransferFunction.h"
+#include "Linear2DTransferFunction_ispc.h"
+
+namespace ospray {
+namespace tfn {
+
+  void Linear2DTransferFunction::commit()
+  {
+    std::cout << "#osp Linear2DTransferFunction" << std::endl;
+
+    // Create the equivalent ISPC transfer function.
+    if (ispcEquivalent == nullptr) createEquivalentISPC();
+
+    // Retrieve the color and opacity values.
+    colorW = getParam1i("colorWidth",  0);
+    colorH = getParam1i("colorHeight", 0);
+    opacityW = getParam1i("opacityWidth",  0);
+    opacityH = getParam1i("opacityHeight", 0);
+    colorValues   = getParamData("colors", nullptr);
+    opacityValues = getParamData("opacities", nullptr);
+    ispc::Linear2DTransferFunction_setPreIntegration(ispcEquivalent,
+						     getParam1i("preIntegration", 0));
+
+    // Set the color values.
+    if (colorValues) {
+      ispc::Linear2DTransferFunction_setColorValues(ispcEquivalent, 
+						    colorValues->numItems, 
+						    (ispc::vec3f*)colorValues->data);
+    }
+
+    // Set the opacity values.
+    if (opacityValues) {
+      ispc::Linear2DTransferFunction_setOpacityValues(ispcEquivalent, 
+						      opacityValues->numItems, 
+						      (float *)opacityValues->data);
+    }
+
+    if (getParam1i("preIntegration", 0) && colorValues && opacityValues)
+      ispc::Linear2DTransferFunction_precomputePreIntegratedValues(ispcEquivalent);
+
+    TransferFunction::commit();
+
+    // Notify listeners that the transfer function has changed.
+    notifyListenersThatObjectGotChanged();
+  }
+
+  std::string Linear2DTransferFunction::toString() const
+  {
+    return "ospray::Linear2DTransferFunction";
+  }
+
+  void Linear2DTransferFunction::createEquivalentISPC()
+  {
+    // The equivalent ISPC transfer function must not exist yet.
+    exitOnCondition(ispcEquivalent != nullptr,
+                    "attempt to overwrite an existing ISPC transfer function");
+
+    // Create the equivalent ISPC transfer function.
+    ispcEquivalent = ispc::Linear2DTransferFunction_createInstance();
+
+    // The object may not have been created.
+    exitOnCondition(ispcEquivalent == nullptr,
+                    "unable to create ISPC transfer function");
+  }
+
+  // A piecewise linear transfer function.
+  OSP_REGISTER_TRANSFER_FUNCTION(Linear2DTransferFunction, piecewise_linear_2d);
+
+}
+} // ::ospray
+
---
+++ ospray/transferFunction/Linear2DTransferFunction.h
@@ -0,0 +1,55 @@
+// ======================================================================== //
+// Copyright 2009-2017 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#pragma once
+
+// ospray
+#include "common/Data.h"
+#include "transferFunction/TransferFunction.h"
+
+namespace ospray {
+namespace tfn {
+
+  /*! \brief A concrete implementation of the TransferFunction class for
+    piecewise linear transfer functions.
+  */
+  struct OSPRAY_SDK_INTERFACE Linear2DTransferFunction : public TransferFunction
+  {
+    Linear2DTransferFunction() = default;
+    virtual ~Linear2DTransferFunction() override = default;
+
+    virtual void commit() override;
+
+    virtual std::string toString() const override;
+
+  private:
+
+    size_t colorW = 0, colorH = 0;
+    size_t opacityW = 0, opacityH = 0;
+
+    //! Data 2D array that stores the color map.
+    Ref<Data> colorValues;
+
+    //! Data 2D array that stores the opacity map.
+    Ref<Data> opacityValues;
+
+    //! Create the equivalent ISPC transfer function.
+    void createEquivalentISPC();
+
+  };
+};
+} // ::ospray
+
